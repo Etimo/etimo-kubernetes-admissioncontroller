980874544cb6d4ea9b8cec3bb9a970613d29681f
Daniel Winther <daniel.kirgiopoulos@gmail.com>
2022-05-10 22:11:43 +0200

    feat: add workflow

--- docker-build-push.sh
+++ docker-build-push.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 docker build . -t etimodanielwinther/etimo-kubernetes-admissioncontroller
+docker tag etimodanielwinther/etimo-kubernetes-admissioncontroller etimodanielwinther/etimo-kubernetes-admissioncontroller:${TAG}
 docker tag etimodanielwinther/etimo-kubernetes-admissioncontroller etimodanielwinther/etimo-kubernetes-admissioncontroller:latest
+docker push etimodanielwinther/etimo-kubernetes-admissioncontroller:${TAG}
 docker push etimodanielwinther/etimo-kubernetes-admissioncontroller:latest
